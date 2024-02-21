ebdd85e26af980bce94355e83a2b2c317979ec00
Zebra <zebra1573@163.com>
2024-02-21 10:35:34 +0800

    2024-02-21

---
+++ Python/19.删除链表的倒数第N个节点.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None)
+        self.val=val
+        self.next=next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummyHead=ListNode(next=head)
+        fast,slow=dummyHead,dummyHead
+        
+        # fast指针要比slow先走n+1步, 这样才能让slow停留在要删除的节点前一个
+        for i in range(n+1):
+            fast=fast.next
+        
+        while fast:
+            fast=fast.next
+            slow=slow.next
+        
+        # 删除倒数第n个节点
+        slow.next=slow.next.next
+        
+        return dummyHead.next
+
