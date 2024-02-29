72d0c9b33aa6a8f3fa35c37c92394e23a11a758d
Zebra <zebra1573@163.com>
2024-02-29 19:35:06 +0800

    2024-02-29

---
+++ Python/1.两数之和.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        result = []  # 存放函数最后返回的结果
+        records = {}  # 存放遍历到的元素, key是nums中的元素, value是该元素在nums中的下标
+
+        # enumerate函数用于创建迭代器, 同时返回元素的索引和值
+        for index, value in enumerate(nums):
+            if target-value in records:
+                result.append(index)
+                temp = records[target - value] # 从字典中取出另一个值的下标
+                result.append(temp)
+            else:
+                records[value] = index # 将遍历的nums下标存入records字典中, nums元素值作为index
+        return result
+
+
+if __name__ == "__main__":
+    nums = [2,7,11,15]
+    target = 9
+    solution = Solution()
+    result = solution.twoSum(nums, target)
+    print(result)
