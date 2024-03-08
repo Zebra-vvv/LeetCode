1e1600952d2b7bb543cf39230752cac18fa0e9dc
Zebra <zebra1573@163.com>
2024-03-08 17:17:10 +0800

    2024-03-08

---
+++ Python/232.用栈实现队列.py
@@ -0,0 +1,47 @@
+class MyQueue:
+
+    def __init__(self):
+        # python的栈可以用List来实现，Python的List提供了足够的方法来模拟栈的所有操作
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x: int) -> None:
+        self.stack_in.append(x)
+
+    def pop(self) -> int:
+        if self.empty():
+            return None
+        if self.stack_out:
+            return self.stack_out.pop()  # 这里调用的都是List的pop方法，而不是自身
+        else:
+            for i in range(len(self.stack_in)):
+                self.stack_out.append(self.stack_in.pop())
+            return self.stack_out.pop()
+
+    def peek(self) -> int:
+        # 复用pop的代码, 只要再append回去就行
+        ans = self.pop()  # 调用的是MyQueue类的pop
+        self.stack_out.append(ans)
+        return ans
+
+    def empty(self) -> bool:
+        return not (self.stack_in or self.stack_out)
+
+
+if __name__ == "__main__":
+    obj = MyQueue()
+    obj.push(1)
+    obj.push(2)
+    obj.push(3)
+    print(obj.pop())
+    print(obj.peek())
+    obj.pop()
+    obj.pop()
+    print(obj.empty())
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
