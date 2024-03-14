5302f574f71653585237c295cf5977ebf42031dd
Zebra <zebra1573@163.com>
2024-03-14 19:57:15 +0800

    2024-03-14

---
+++ Python/541.反转字符串II.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s_list = list(s)
+        for i in range(0, len(s_list), 2 * k):
+
+            # 1. 每隔 2k 个字符的前 k 个字符进行反转
+            # 2. 剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符
+            # 1和2两种情况都可以用 if 中的代码执行(因为都是反转前k个)
+            if i + k <= len(s_list):
+                s_list[i:i+k] = reversed(s_list[i:i+k])
+
+            else:
+                # 3. 剩余字符少于 k 个，则将剩余字符全部反转。
+                s_list[i:] = reversed(s_list[i:])
+        # 将列表s_list中的所有元素连接成一个字符串, ''是连接字符串的分隔符，这里使用空字符串表示不需要分隔符
+        return ''.join(s_list)
+
+
+if __name__ == "__main__":
+    str = "abcdefg"
+    k = 2
+    solution = Solution()
+    result = solution.reverseStr(str, k)
+    print(result)
