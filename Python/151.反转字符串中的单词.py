4242ddc17f1980c0c0e085eb9dd4a2fd84edbb04
Zebra <zebra1573@163.com>
2024-03-24 15:50:23 +0800

    2023-03-24

--- Python/151.反转字符串中的单词.py
+++ Python/151.反转字符串中的单词.py
@@ -1,26 +1,21 @@
 class Solution:
-    # def reverseWords(self, s: str) -> str:
+    def reverseWords(self, s: str) -> str:
+        # 将字符串拆分为单词，即转换成列表类型
+        words = s.split()
 
-    def removeExtraSpaces(self, s: str) -> str:
-        slow, fast = 0, 0    # 
-        s = list(s)
-        while fast < len(s):
-            if s[fast] != ' ':
-                if slow != 0:         # 第一个单词之前不用留空格
-                    s[slow] = ' '     # 每个单词之前留1个空格
-                    slow += 1
-                while fast < len(s) and s[fast] != ' ':
-                    s[slow] = s[fast]
-                    slow += 1
-                    fast += 1
-            else:
-                fast += 1
-        return ''.join(s[:slow])
+        # 双指针反转单词之间的顺序
+        left, right = 0, len(words) - 1
+        while left < right:
+            words[left], words[right] = words[right], words[left]
+            left += 1
+            right -= 1
 
-        
+        # 将列表转换成字符串, 中间用空格连接
+        return " ".join(words)
+     
 if __name__=="__main__":
     # 测试
-    s = "   hello   world   "
-    solution=Solution()
-    res = solution.removeExtraSpaces(s)
+    s = "the sky is blue"
+    solution = Solution()
+    res = solution.reverseWords(s)
     print(res)  # 输出: "hello world"
