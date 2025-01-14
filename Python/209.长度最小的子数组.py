cd3c2aed6b12c3437614b6f1d531b0a6796abcaf
Zebra <zebra1573@163.com>
2025-01-14 17:01:51 +0800

    2025-01-14 二刷

--- Python/209.长度最小的子数组.py
+++ Python/209.长度最小的子数组.py
@@ -3,14 +3,15 @@ from typing import List
 # 滑动窗口法（双指针法）
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        l = len(nums)
         left, right = 0, 0
         min_len = float('inf')  # min_len设置为无穷大, 确保下面min函数能找到最小值
         sum = 0
-        while right < l:
+        while right < len(nums):
             sum += nums[right]
             while sum >= target:
-                min_len = min(min_len, right-left+1)
+                min_len = min(min_len, right - left + 1)
+                
+                # 刚刚加上一个右边界的数, 够了, 尝试从左边界去掉一个数, 看看还够不够
                 sum -= nums[left]
                 left += 1
             right += 1
@@ -19,7 +20,7 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    nums: List[int] = [2, 3, 1, 2, 4, 3]
+    nums = [2, 3, 1, 2, 4, 3]
     target = 7
     res = solution.minSubArrayLen(target, nums)
     print(res)
