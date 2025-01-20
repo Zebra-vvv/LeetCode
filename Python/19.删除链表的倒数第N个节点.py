6ee51e8e3b5d40641530052549598f86fcd96d85
Zebra <zebra1573@163.com>
2025-01-20 15:39:12 +0800

    2025-01-20 二刷

--- Python/19.删除链表的倒数第N个节点.py
+++ Python/19.删除链表的倒数第N个节点.py
@@ -6,46 +6,30 @@ class ListNode:
         self.next=next
 
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummyHead=ListNode(next=head)
-        fast,slow=dummyHead,dummyHead
+    def removeNthFromEnd(self, head:ListNode, n:int) -> ListNode:
+        dummyHead = ListNode()
+        dummyHead.next = head
         
-        # fast指针要比slow先走n+1步, 这样才能让slow停留在要删除的节点前一个
-        for i in range(n+1):
-            fast=fast.next
-        
-        while fast:
-            fast=fast.next
-            slow=slow.next
-        
-        # 删除倒数第n个节点
-        slow.next=slow.next.next
-        
+        fast, slow = dummyHead, dummyHead
+        for i in range(n):
+            fast = fast.next
+        while fast.next: # 这里要判断fast.next, 纸笔模拟就知道了, 这样slow停下的位置正好
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
         return dummyHead.next
 
+
 if __name__ == "__main__":
-    # 创建一个链表: 1 -> 2 -> 3 -> 4 -> 5
-    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3) 
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next  = ListNode(5)
 
-    # 打印原始链表
-    print("Original Linked List:")
-    cur = head
-    while cur:
-        print(cur.val, end=" -> ")
-        cur = cur.next
-    print("None")
-
-    # 创建 Solution 实例
     solution = Solution()
+    head = solution.removeNthFromEnd(head, 2)
 
-    # 删除倒数第2个节点
-    n = 2
-    new_head = solution.removeNthFromEnd(head, n)
-
-    # 打印删除后的链表
-    print(f"\nLinked List after removing {n}th node from the end:")
-    cur = new_head
-    while cur:
-        print(cur.val, end=" -> ")
-        cur = cur.next
-    print("None")
+    while head:
+        print(head.val)
+        head = head.next
