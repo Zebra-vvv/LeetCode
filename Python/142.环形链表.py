cb0c40ae1e77c94c4407bc9cb322f7e1ed801cfd
Zebra <zebra1573@163.com>
2025-02-06 19:00:47 +0800

    2025-02-06 二刷

--- Python/142.环形链表.py
+++ Python/142.环形链表.py
@@ -1,30 +1,27 @@
-from typing import Optional
-
-
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val = 0, next = None):
         self.val = val
         self.next = next
 
-
 class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def detectCycle(self, head:ListNode) -> ListNode:
         slow, fast = head, head
 
-        while fast and fast.next:
+        while fast and fast.next: # 这里不能用fast.next.next, 可能会访问到None.next
             slow = slow.next
             fast = fast.next.next
-            
-            if slow == fast:
-                index = slow
 
-                while index != head: # 直接在while中判断，不会漏判
-                    index = index.next
+            # 如果相遇, 表示链表中有环
+            if slow == fast:
+                index = slow # index是环中相遇点
+                
+                # 记住数学结论: head到环的入口距离 = index到环的入口的距离
+                while head != index:
                     head = head.next
+                    index = index.next
                 return index
         return None
-
-
+        
 if __name__ == "__main__":
     # 创建链表节点
     node1 = ListNode(3)
@@ -36,12 +33,9 @@ if __name__ == "__main__":
     node1.next = node2
     node2.next = node3
     node3.next = node4
-    node4.next = node2  # 这里构造一个环
-
+    node4.next = node2  # 这里构造一个环, 记住next要指向节点本身而不是节点的val
+    
     solution = Solution()
     result = solution.detectCycle(node1)
+    print(result.val)
 
-    if result:
-        print(f"链表中存在环，入口点的值为 {result.val}")
-    else:
-        print("链表中不存在环")
