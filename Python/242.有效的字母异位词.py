0bb3c69969bfa6d7b411cba5556eab80ffc801fb
Zebra <zebra1573@163.com>
2025-02-12 11:48:52 +0800

    2025-02-12 二刷

--- Python/242.有效的字母异位词.py
+++ Python/242.有效的字母异位词.py
@@ -3,7 +3,7 @@ class Solution:
         record = [0] * 26
         # 字符串可以直接这样遍历
         for i in s:
-            # ord 函数用于获取字符的 ASCII 值
+            # ord 函数用于获取字符的 ASCII 值, 因为是函数, 所以要用()
             # 在ASCII编码中，小写字母a的ASCII值是97，而字母'a'对应的数组索引应该是0。
             # 因此，如果我们用字母的ASCII值减去字母'a'的ASCII值，我们就可以得到字母在数组中的正确索引。
             # 例如：对于字母'a'，ord('a') - ord('a') 的结果是0，'a'在record确实应该在第一个位置
