508d168e54cf8406c6a249bc63616901d14ce565
Zebra <zebra1573@163.com>
2025-04-16 17:32:16 +0800

    2025-04-16 二刷

--- Python/349.两个数组的交集.py
+++ Python/349.两个数组的交集.py
@@ -1,22 +1,38 @@
 from typing import List
 
-
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        set1 = set()
+# 线性查找 O(m × n)
+class Solution1:
+    def intersection (self, nums1:List[int], nums2:List[int]) -> List[int]:
+        result = set()
+        for i in nums1:
+            if i in nums2:
+                result.add(i)
+        return list(result)
+    
+# 哈希查找 O(m + n)
+class Solution2:
+    def intersection (self, nums1:List[int], nums2:List[int]) -> List[int]:
+       
         result_set = set()
+        
+        # 先把nums1的元素放到set中去, 在set中查找元素的时间复杂度是 O(1), 因为它是基于哈希表实现的
+        set1 = set()
         for i in nums1:
             set1.add(i)
+
+        # 在set1中寻找nums2元素
         for i in nums2:
             if i in set1:
                 result_set.add(i)
-        result_list = list(result_set)  # 函数要求返回值为列表, 将集合转换为列表
-        return result_list
 
+        # 函数要求返回值为列表, 将集合转换为列表
+        result_list = list(result_set)  
+        return result_list
 
 if __name__ == "__main__":
     nums1 = [1, 2, 3, 1]
     nums2 = [2, 3]
-    solution = Solution()
-    result = solution.intersection(nums1, nums2)
-    print(result)
+    solution = Solution2()
+    res = solution.intersection(nums1, nums2)
+    print(res)
+
