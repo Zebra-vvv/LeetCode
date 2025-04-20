f9c075e6f1119371ea7a42300f372661d3b69c81
Zebra <zebra1573@163.com>
2025-04-20 18:10:09 +0800

    2025-04-20 二刷

--- Python/383.赎金信.py
+++ Python/383.赎金信.py
@@ -1,12 +1,13 @@
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+    def canConstruct(self, ransom:str, magazine:str) -> bool:
+        # 和 242.有效字母的异位词 思路几乎一样
         record = [0] * 26
-        for i in ransomNote:
-            record[ord(i)-ord('a')] += 1
-        for j in magazine:
+        for i in magazine:
+            record[ord(i)-ord('a')] += 1 
+        for j in ransom:
             record[ord(j)-ord('a')] -= 1
-        for k in record:
-            if k > 0:
+        for val in record:
+            if val < 0:
                 return False
         return True
 
