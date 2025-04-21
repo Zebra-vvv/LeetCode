84a97188b28a8f5c928d66ba1d2ab68780b83d7f
Zebra <zebra1573@163.com>
2025-04-21 10:33:23 +0800

    2025-04-21 二刷

--- Python/15.三数之和.py
+++ Python/15.三数之和.py
@@ -1,45 +1,32 @@
 from typing import List
 
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        nums.sort()
-
+        res = []
+        nums = sorted(nums)
+        print(nums)
         for i in range(len(nums)):
-            # 如果第一个元素已经大于0，不可能找到结果
-            if nums[i] > 0:
-                return result
-
-            # 如果nums[i] == nums[i - 1], 那么这一轮的结果在上一轮都已收集了, 直接遍历下一轮
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1]: # 跳过重复的 i(因为要访问nums[i - 1], 为了防止越界, 所以必须判断 i > 0)
                 continue
-
-            left, right = i + 1, len(nums) - 1
-
-            while left < right:
-                sum = nums[i] + nums[left] + nums[right]
-                if sum < 0:
-                    left += 1
-                elif sum > 0:
-                    right -= 1
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] > 0:   
+                    k -= 1 
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
                 else:
-                    result.append([nums[i], nums[left], nums[right]])
-
-                    # 和上面的去重逻辑相同, 值和之前一轮的相等就不需要重复收集结果了
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-
-                    right -= 1
-                    left += 1
-
-        return result
-
-
+                    res.append([nums[i], nums[j], nums[k]])
+                    k -= 1
+                    j += 1
+                    while j < k and nums[j] == nums[j - 1]:  # 去重, 跳过重复的 j
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:  # 去重, 跳过重复的 k
+                        k -= 1
+        return res
+        
 if __name__ == "__main__":
     nums = [-1, 0, 1, 2, -1, -4]
     solution = Solution()
-    result = solution.threeSum(nums)
-    print(result)
+    res = solution.threeSum(nums)
+    print(res)
