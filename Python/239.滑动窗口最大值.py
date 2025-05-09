0a8b4b4550f952d32c2e818158b4fdb5ce9541ee
Zebra <zebra1573@163.com>
2025-05-09 17:33:18 +0800

    2025-05-09 二刷

--- Python/239.滑动窗口最大值.py
+++ Python/239.滑动窗口最大值.py
@@ -6,18 +6,20 @@ class Myqueue:
     def __init__(self):
         self.queue = deque()
 
-    def pop(self, value):
-        # 当前遍历值等于单调栈最左侧的值（最大值），说明当前元素真的需要被pop掉
-        if self.queue and self.queue[0] == value:
-            self.queue.popleft()
-
     def push(self, value):
+        
+        # 如果value比前面的值都大, 就持续抛弃前面的值, 因为较小值没有必要维护
         while self.queue and self.queue[-1] < value:
             self.queue.pop()
         self.queue.append(value)
 
+    def pop(self, value):
+        # 当前遍历值等于单调栈最左侧（队头，最大值）的值，说明当前元素真的需要手动pop掉(较小值在较大值push时就会被抛弃)
+        if self.queue and self.queue[0] == value:
+            self.queue.popleft()
+
     def getMaxValue(self):
-        return self.queue[0]
+        return self.queue[0] # 最左侧的元素（队头）
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
@@ -27,12 +29,12 @@ class Solution:
         # 先将前k个元素放入单调栈中
         for i in range(k):
             myqueue.push(nums[i])
-        # 先记录前k个元素的最大值
+        # 记录第一个滑动窗口的最大值
         result.append(myqueue.getMaxValue())
 
-        # 遍历k个之后的元素
+        # 开始移动滑动窗口
         for i in range(k, len(nums)):
-            myqueue.pop(nums[i - k])  # 移除窗口最左侧的元素
+            myqueue.pop(nums[i - k])  # 弹出窗口最左侧的那个元素（可能早就弹出了）
             myqueue.push(nums[i])  # 将当前元素加入单调队列
             result.append(myqueue.getMaxValue())  # 记录当前窗口内的最大值
         return result
