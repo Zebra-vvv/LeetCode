2a6a01523f9b04e161863d8cace162602e8db81e
Zebra <zebra1573@163.com>
2025-05-14 16:42:40 +0800

    2025-05-14

--- Python/347.前K个高频元素.py
+++ Python/347.前K个高频元素.py
@@ -1,33 +1,28 @@
-# 对元素的频率进行排序需要用到 map ，key就是元素值,value是元素出现的次数
-# 题目只要求拿到前k个高频元素，所以不需要对所有元素进行排序，只需要维护一个大小为k的堆即可
-# 必须维护小顶堆而不是大顶堆，因为遍历所有元素时，新加入元素会不断弹出最小的堆顶元素，最后剩下的就是最大的k个元素
-
 from typing import List
+import heapq # Python 中的堆操作模块
 
-# Python 中的一个模块，提供了对堆数据结构的支持
-import heapq
-
-
+# 对元素的频率进行排序需要用到 map ，key就是元素值，value是元素出现的次数
+# 题目只要求拿到前k个高频元素，所以不需要对所有元素进行排序，只需要维护一个大小为k的堆即可
+# 这题需要用小顶堆，因为小顶堆的特性符合需要（不断把堆顶较小的元素pop出去，留下的反而是较大的元素）
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-        # 统计频率
-        freq_map = {}
+        # 利用map统计所有元素的出现频率
+        map = {}
         for i in range(len(nums)):
-            freq_map[nums[i]] = freq_map.get(nums[i], 0) + 1
+            map[nums[i]] = map.get(nums[i], 0) + 1
 
-        pri_que = []  # 小顶堆
-
-        # 用固定大小为k的小顶堆，扫描所有频率的数值
-        for key, frep in freq_map.items():
-            # 将 freq_map 中的一个元组 (freq, key) 添加到堆 pri_que 中，并保持堆的性质，以元组的第一个元素进行排序，默认就是小顶堆
-            heapq.heappush(pri_que, (frep, key))
-            if len(pri_que) > k:
-                heapq.heappop(pri_que)
+        # 利用小顶堆heap来找出前k个高频元素
+        heap = []  # 用List建立小顶堆即可，heapq模块可以帮忙维持堆结构
+        for key, freq in map.items():
+            # 将 map 中的一个元组 (freq, key) 添加到小顶堆 heap 中，并保持堆的性质，默认以元组的第一个元素进行排序，所以把freq放在前面
+            heapq.heappush(heap, (freq, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
         
         result = []
-        while pri_que:
-            result.append(heapq.heappop(pri_que)[1])  # 只取元组的第二个元素，即 key
+        while heap:
+            result.append(heapq.heappop(heap)[1])  # 只取元组的第2个元素，即 key
         # python 切片操作，其基本语法形式为 [start:stop:step]
         return result[::-1]  # 由于小顶堆的特性，需要逆序输出才是按照频率从高到低的顺序
     
