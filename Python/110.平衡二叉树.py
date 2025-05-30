46f68f93c793e54f974405d32a45dc0d4ea3131b
Zebra <zebra1573@163.com>
2025-05-30 14:47:07 +0800

    2025-05-30

---
+++ Python/110.平衡二叉树.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self, val=0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root:TreeNode) -> bool:
+        if self.dfs(root) != -1:
+            return True
+        else:
+            return False
+    
+    def dfs(self, node) -> int:
+        if not node:
+            return 0
+      
+        # 后序遍历
+        leftHeight= self.dfs(node.left) # 左
+        rightHeight= self.dfs(node.right) # 右
+
+        # 中
+        # 如果左子树或者右子树已经不平衡了，无需继续遍历，直接返回-1
+        if leftHeight == -1 or rightHeight == -1 or abs(leftHeight - rightHeight) > 1:
+            return -1 # 返回-1：当前节点的左右子树不平衡
+        
+        return max(leftHeight, rightHeight) + 1 # 返回当前节点高度：左右子树中高的那个再+1
+    
+if __name__ == "__main__":
+    #        1
+    #      /   \
+    #     2     3
+    #    / \   /
+    #   4   5 6
+    node4 = TreeNode(4)
+    node5 = TreeNode(5)
+    node6 = TreeNode(6)
+    node2 = TreeNode(2, node4, node5)
+    node3 = TreeNode(3, node6)
+    root = TreeNode(1,node2,node3)
+
+    solution = Solution()
+    result = solution.isBalanced(root)
+    print(result)
---
+++ Python/222.完全二叉树的节点个数.py
@@ -0,0 +1,42 @@
+class TreeNode:
+    def __init__(self, val=0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right 
+
+class Solution:
+    def countNodes(self, root:TreeNode) -> int:
+        if not root:
+            return 0
+        return self.dfs(root)
+   
+    # 这种问题使用后序遍历更加通用
+    # 比如：求树的最大深度、求是否平衡、判断对称结构
+    def dfs(self, node) -> int:
+        if not node:
+            return 0
+        
+        # 后序遍历
+        leftNum = self.dfs(node.left) # 左
+        rightNum = self.dfs(node.right) # 右
+        treeNum = leftNum + rightNum + 1 # 中，在“中“的处理逻辑中加和
+        return treeNum
+    
+if __name__ == "__main__":
+    # 构造一个完全二叉树：
+    #        1
+    #      /   \
+    #     2     3
+    #    / \   /
+    #   4   5 6
+
+    node4 = TreeNode(4)
+    node5 = TreeNode(5)
+    node6 = TreeNode(6)
+    node2 = TreeNode(2, node4, node5)
+    node3 = TreeNode(3, node6)
+    root = TreeNode(1,node2,node3)
+
+    solution = Solution()
+    count = solution.countNodes(root)
+    print("节点数:", count)  # 预期输出为 6
