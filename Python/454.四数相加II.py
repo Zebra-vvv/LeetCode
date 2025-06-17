fbc5447ae473fc37b7488fc4ecca7861a5cf51ff
Zebra <zebra1573@163.com>
2025-06-17 16:43:47 +0800

    2025-06-17

--- Python/454.四数相加II.py
+++ Python/454.四数相加II.py
@@ -1,10 +1,12 @@
 from typing import List
 
-
+# 一句话总结：
+# 先用哈希表记录前两个数组所有和的出现次数，再遍历后两个数组，查找使四数之和为0的互补值，从而统计满足条件的组合个数。
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
 
         records = {}  # key:a+b的数值, value:a+b数值出现的次数
+        count = 0 # 满足四数相加等于0的组合个数
 
         for a in nums1:
             for b in nums2:
@@ -12,12 +14,13 @@ class Solution:
                     records[a+b] += 1
                 else:
                     records[a+b] = 1  # 如果还没有a+b这个键值，必须先初始化为1
-        count = 0 # 满足四数相加等于0的组合个数
+
         for c in nums3:
             for d in nums4:
-                target = 0-(c+d)
+                target = 0-(c+d)      # target：期望在records中找到的值
                 if target in records: # 在records中寻找目标值
                     count += records[target] # 找到几个target就必须加上几个count
+
         return count
 
 
